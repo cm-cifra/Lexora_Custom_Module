17f438d367493bb6590cbcf3c1c54f0a391c0660
chris mark cifra <ccifra@umindanao.edu.ph>
2025-08-06 03:26:48 +0800

    Add files via upload

---
+++ kits_configuration/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Product Configuration 2.0",
+    'version': '2.0',
+    'category': 'Website',
+    "depends": ["website","product"],
+    'author': 'chris mark cifra',
+    'description': 'Product Configuration of Cabinets.',
+    'data': [
+        'views/product_selection.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
