f20e4c5061959e08e95cb15e988c856a493b5f25
chris mark cifra <ccifra@umindanao.edu.ph>
2025-08-09 01:44:44 +0800

    Add files via upload

--- sales_status_tracker/models/order_tracking.py
+++ sales_status_tracker/models/order_tracking.py
@@ -18,15 +18,23 @@ class SaleOrder(models.Model):
         compute="_compute_delivery_info",
         store=False
     )
+    carrier_tracking_ref = fields.Char(
+        string="Carrier Tracking Ref",
+        compute="_compute_delivery_info",
+        store=False
+    )
 
     def _compute_delivery_info(self):
         for order in self:
-            pickings = order.picking_ids.filtered(lambda p: p.state == 'done')
+            pickings = order.picking_ids.filtered(lambda p: p.state in ['assigned', 'done', 'confirmed'])
             if pickings:
-                order.delivery_date = pickings[-1].date_done
-                order.carrier_name = pickings[-1].carrier_id.name
-                order.expected_delivery_date = pickings[-1].scheduled_date
+                last_picking = pickings[-1]
+                order.delivery_date = last_picking.date_done
+                order.expected_delivery_date = last_picking.scheduled_date
+                order.carrier_name = last_picking.carrier_id.name
+                order.carrier_tracking_ref = last_picking.carrier_tracking_ref
             else:
                 order.delivery_date = False
                 order.expected_delivery_date = False
                 order.carrier_name = False
+                order.carrier_tracking_ref = False
