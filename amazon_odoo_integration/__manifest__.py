aaf88148640233537b24e1bb15677c306a08a73c
chris mark cifra <ccifra@umindanao.edu.ph>
2025-08-12 02:30:43 +0800

    Add files via upload

---
+++ amazon_odoo_integration/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Amazon Connector (starter)",
+    "version": "1.0.0",
+    "summary": "Sync Amazon Products and Sales Orders by account name and marketplace",
+    "author": "You / Your Company",
+    "depends": ["sale", "product"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/amazon_account_views.xml",
+        "views/amazon_mapping_views.xml",
+        "data/cron_data.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
