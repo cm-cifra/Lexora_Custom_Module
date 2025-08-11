a002106fbad44e59f5a499407fb3327187cdef55
chris mark cifra <ccifra@umindanao.edu.ph>
2025-08-12 03:07:16 +0800

    Add files via upload

--- amazon_odoo_integration/__manifest__.py
+++ amazon_odoo_integration/__manifest__.py
@@ -3,7 +3,7 @@
     "version": "1.0.0",
     "summary": "Sync Amazon Products and Sales Orders by account name and marketplace",
     "author": "You / Your Company",
-    "depends": ["sale", "product"],
+    "depends": ["sale", "product", "base"],   
     "data": [
         "security/ir.model.access.csv",
         "views/amazon_account_views.xml",
