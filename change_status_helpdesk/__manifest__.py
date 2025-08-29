2337ef575211b7d33a81c8fb7352e819ee0ad3d4
chris mark cifra <ccifra@umindanao.edu.ph>
2025-08-30 01:12:58 +0800

    Add files via upload

---
+++ change_status_helpdesk/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Helpdesk Charge Status Confirmation",
+    "version": "16.0.1.0.0",
+    "summary": "Confirmation dialog when setting charge status to disputed",
+    "depends": ["helpdesk"],
+    "data": [
+        "views/helpdesk_ticket_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
