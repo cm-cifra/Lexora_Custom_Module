b48026a99198e730ddce4bc18f80260f7af927ad
chris mark cifra <ccifra@umindanao.edu.ph>
2025-08-30 01:19:56 +0800

    Update __manifest__.py

--- change_status_helpdesk/__manifest__.py
+++ change_status_helpdesk/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Helpdesk Charge Status Confirmation",
-    "version": "16.0.1.0.0",
+    "version": "16.0.1.0",
     "summary": "Confirmation dialog when setting charge status to disputed",
     "depends": ["helpdesk"],
     "data": [
