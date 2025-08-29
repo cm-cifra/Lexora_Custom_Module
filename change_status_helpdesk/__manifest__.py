98e9b53fcc9ea4d00cafa19af33c830b0c66d319
chris mark cifra <ccifra@umindanao.edu.ph>
2025-08-30 00:09:44 +0800

    Add files via upload

---
+++ change_status_helpdesk/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Charge Status Confirmation (Helpdesk)",
+    "version": "1.0",
+    "summary": "Require Yes/No confirmation when setting charge status to Disputed on helpdesk tickets",
+    "author": "You", 
+    "category": "Helpdesk",
+    "license": "LGPL-3",
+    "depends": ["helpdesk"],
+    "data": [
+        "views/helpdesk_ticket_view.xml",
+        "views/dispute_wizard_view.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
