98b13f691b7f8871c5a7c8050ed819a979dcd3e9
chris mark cifra <ccifra@umindanao.edu.ph>
2025-09-01 22:49:03 +0800

    Add files via upload

--- change_status_helpdesk/__manifest__.py
+++ change_status_helpdesk/__manifest__.py
@@ -1,11 +1,16 @@
 {
-    "name": "Helpdesk Ticket Charge Status Warning",
-    "version": "1.0",
-    "author": "chris mark cifra",
-    "depends": ["helpdesk"],
-    "data": [
-        "views/helpdesk_ticket_views.xml",
-        "views/approval_warning_views.xml",
-    ],
-    "installable": True,
+    'name': 'Helpdesk Ticket Alert',
+    'version': '1.0',
+    'summary': 'Show alert when charge status changes to Approved',
+    'description': """
+        This module adds an alert message when the field 
+        x_studio_charge_status in helpdesk.ticket is changed to "Approved".
+    """,
+    'category': 'Helpdesk',
+    'author': 'chris mark cifra',
+    'depends': ['helpdesk'],
+    'data': [],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
 }
--- change_status_helpdesk/models/charge_status.py
+++ change_status_helpdesk/models/charge_status.py
@@ -1,35 +1,13 @@
-from odoo import models, api, _
+from odoo import models
 
 class HelpdeskTicket(models.Model):
-    _inherit = "helpdesk.ticket"
+    _inherit = 'helpdesk.ticket'
 
-    @api.onchange("x_studio_charge_status")
-    def _onchange_charge_status_warning(self):
-        """Show an immediate yellow warning banner when the field becomes 'Approved'."""
-        if self.x_studio_charge_status == "Approved":  # case-sensitive
-            return {
-                "warning": {
-                    "title": _("Charge Status Approved"),
-                    "message": _(
-                        "This ticket's charge status is set to APPROVED. Please review carefully."
-                    ),
-                }
-            }
-
-    def action_check_charge_status(self):
-        """Open a modal popup wizard if the field is 'Approved'."""
-        self.ensure_one()
-        if self.x_studio_charge_status == "Approved":
-            return {
-                "type": "ir.actions.act_window",
-                "res_model": "helpdesk.approval.warning",
-                "view_mode": "form",
-                "target": "new",
-                "context": {
-                    "default_message": _(
-                        "Charge status is APPROVED for ticket %s."
-                    ) % (self.display_name or self.name),
-                },
-            }
-        # Nothing to show
-        return True
+    def write(self, vals):
+        res = super().write(vals)
+        if 'x_studio_charge_status' in vals and vals['x_studio_charge_status'] == 'Approved':
+            self.env.user.notify_info(
+                message="Charge status changed to Approved!",
+                title="Information"
+            )
+        return res
