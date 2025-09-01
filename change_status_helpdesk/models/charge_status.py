77f977821aa41b51fed61d076d7c5d5e5539d52c
chris mark cifra <ccifra@umindanao.edu.ph>
2025-09-01 22:56:42 +0800

    Update charge_status.py

--- change_status_helpdesk/models/charge_status.py
+++ change_status_helpdesk/models/charge_status.py
@@ -1,16 +1,16 @@
-from odoo import models, _
+from odoo import models, api, _
+from odoo.exceptions import UserError
 
 class HelpdeskTicket(models.Model):
     _inherit = 'helpdesk.ticket'
 
-    def write(self, vals):
-        res = super().write(vals)
-        if 'x_studio_charge_status' in vals and vals['x_studio_charge_status'] == 'Approved':
-            # Send a notification to the current user
-            message = _("Charge status changed to Approved!")
-            self.env['bus.bus']._sendone(
-                self.env.user.partner_id,     # recipient
-                'simple_notification',        # channel
-                {'title': "Information", 'message': message, 'sticky': False}
-            )
-        return res
+    @api.onchange('x_studio_charge_status')
+    def _onchange_charge_status(self):
+        if self.x_studio_charge_status == 'Approved':
+            # Blocking popup
+            return {
+                'warning': {
+                    'title': _("Information"),
+                    'message': _("⚠️ The charge status has been set to Approved!"),
+                }
+            }
