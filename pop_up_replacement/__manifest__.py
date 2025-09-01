7b8629aafea6feb54abb748def7a0d439f9bceec
chris mark cifra <ccifra@umindanao.edu.ph>
2025-09-02 00:08:58 +0800

    Add files via upload

---
+++ pop_up_replacement/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Helpdesk Replacement Warning',
+    'version': '16.0.1.0.0',
+    'summary': 'Show popup warning when Replacement tag is set on Helpdesk Ticket',
+    'description': """
+        This module displays a popup warning whenever a Helpdesk Ticket 
+        has a tag containing the word 'Replacement'.
+    """,
+    'author': 'chris mark cifra',
+    'depends': ['helpdesk'],
+    'data': [],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
---
+++ pop_up_replacement/models/kits.py
@@ -0,0 +1,15 @@
+from odoo import models, api
+
+class HelpdeskTicket(models.Model):
+    _inherit = 'helpdesk.ticket'
+
+    @api.onchange('tag_ids')
+    def _onchange_tag_ids_show_warning(self):
+        """ Show popup if 'Replacement' tag is selected """
+        if self.tag_ids.filtered(lambda t: 'Replacement' in t.name):
+            return {
+                'warning': {
+                    'title': "Replacement Tag Selected",
+                    'message': "This ticket is tagged with 'Replacement'. Please review carefully.",
+                }
+            }
