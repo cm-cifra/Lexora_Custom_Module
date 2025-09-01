9b0dbb86c270402e8a44d682826658ae60ad474e
chris mark cifra <ccifra@umindanao.edu.ph>
2025-09-01 22:09:28 +0800

    Update charge_status.py

--- change_status_helpdesk/models/charge_status.py
+++ change_status_helpdesk/models/charge_status.py
@@ -3,16 +3,15 @@ from odoo import models, api, _
 class HelpdeskTicket(models.Model):
     _inherit = 'helpdesk.ticket'
 
-    def action_check_charge_status(self):
-        for rec in self:
-            if rec.x_studio_charge_status == 'approved':
-                return {
-                    'type': 'ir.actions.act_window',
-                    'res_model': 'helpdesk.approval.warning',
-                    'view_mode': 'form',
-                    'target': 'new',
-                    'context': {
-                        'default_message': _("Charge status is APPROVED for ticket %s.") % rec.name,
-                    }
+    @api.onchange('x_studio_charge_status')
+    def _onchange_charge_status(self):
+        if self.x_studio_charge_status == "Approved":
+            return {
+                'type': 'ir.actions.act_window',
+                'res_model': 'helpdesk.approval.warning',
+                'view_mode': 'form',
+                'target': 'new',
+                'context': {
+                    'default_message': _("Charge status is APPROVED for ticket %s.") % self.name,
                 }
-        return True
+            }
