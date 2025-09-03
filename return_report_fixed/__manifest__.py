f675b6a52c3d94b381c926fa34a2a51b78ac360a
chris mark cifra <ccifra@umindanao.edu.ph>
2025-09-04 02:18:27 +0800

    Update __manifest__.py

--- return_report_fixed/__manifest__.py
+++ return_report_fixed/__manifest__.py
@@ -1,6 +1,6 @@
 {
-    "name": "Return Report",
-    "version": "17.0.1.0.0",
+    "name": "Return Reports",
+    "version": "1.0",
     "summary": "Custom Return Report Application",
     "category": "Reporting",
     "author": "Your Company",
