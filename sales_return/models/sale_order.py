9673305114f391a269fbfa59f787693c14b0d936
chris mark cifra <ccifra@umindanao.edu.ph>
2025-09-05 00:53:19 +0800

    Update sale_order.py

--- sales_return/models/sale_order.py
+++ sales_return/models/sale_order.py
@@ -3,25 +3,19 @@ from odoo import models, fields, api
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    # Add new return states to the selection
-    state = fields.Selection(selection_add=[
-        ('return_initiated', 'Return Initiated'),
-        ('returned', 'Returned')
-    ])
-
-    # Boolean field to identify return orders
+    # Boolean field to identify return orders based on order_state
     is_return_order = fields.Boolean(
         string="Is Return Order",
         compute="_compute_is_return_order",
         store=True
     )
 
-    @api.depends('state')
+    @api.depends('order_state')
     def _compute_is_return_order(self):
         for order in self:
-            order.is_return_order = order.state in ['return_initiated', 'returned']
+            order.is_return_order = order.order_state in ['return_initiated', 'returned']
 
     # Method to get all return orders
     @api.model
     def get_return_orders(self):
-        return self.search([('state', 'in', ['return_initiated', 'returned'])])
+        return self.search([('order_state', 'in', ['return_initiated', 'returned'])])
