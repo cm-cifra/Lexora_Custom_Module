12b7cc35a374767ec4c5377dcd3b256c9ad9c69d
chris mark cifra <ccifra@umindanao.edu.ph>
2025-09-10 02:56:07 +0800

    Add files via upload

---
+++ sale_order_date_filter/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Sale Order Sort Dropdown",
+    "version": "1.0",
+    "category": "Sales",
+    "summary": "Adds a sort dropdown beside search in Sale Orders",
+    "author": "Your Name",
+    "depends": ["sale"],  # in Odoo 17, use sale_management
+    "data": [
+        "views/sale_order_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
