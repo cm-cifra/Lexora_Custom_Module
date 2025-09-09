380c27762aadcf83faf374170dcbf072315f2fa3
chris mark cifra <ccifra@umindanao.edu.ph>
2025-09-10 00:19:38 +0800

    Add files via upload

---
+++ sale_multi_search/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Multi Search Sale PO",
+    "version": "17.0.1.0.0",
+    "summary": "Search multiple PO numbers separated by space in Sale Orders",
+    "author": "Your Name",
+    "license": "LGPL-3",
+    "category": "Sales",
+    "depends": ["sale_management"],
+    "data": [],
+    "installable": True,
+    "application": False,
+}
