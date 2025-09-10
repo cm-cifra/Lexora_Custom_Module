44a62a8c7d685ee15ffca0d8c3f0fca58d3e16aa
chris mark cifra <ccifra@umindanao.edu.ph>
2025-09-11 00:52:00 +0800

    Update __manifest__.py

--- Return_multi_search/__manifest__.py
+++ Return_multi_search/__manifest__.py
@@ -3,7 +3,7 @@
      'version': '1.1',
     'summary': 'Custom search',
     'category': 'Sales',
-    "depends": ["stock", "purchase"],  
+     "depends": ["sale", "stock"],   
     'data': [
         'views/sale_order_views.xml',
     ],
