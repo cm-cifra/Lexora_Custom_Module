19103a9d3c19f3a85a6ab89b0cfd49ce50996411
chris mark cifra <ccifra@umindanao.edu.ph>
2025-09-12 02:57:51 +0800

    Update quality_check.py

--- quality_return/models/quality_check.py
+++ quality_return/models/quality_check.py
@@ -5,15 +5,15 @@ from odoo import models, fields
 class StockMoveLine(models.Model):
     _inherit = "stock.move.line"
 
-    is_return_location = fields.Boolean(
-        string="Is Return",
-        compute="_compute_is_return",
+    is_return_reference = fields.Boolean(
+        string="Is Return (Reference)",
+        compute="_compute_is_return_reference",
         store=False,
     )
 
-    def _compute_is_return(self):
-        """Mark move lines whose destination location is a return location."""
+    def _compute_is_return_reference(self):
+        """Mark move lines as return if reference starts with WH/IN/RETURN"""
         for rec in self:
-            rec.is_return_location = bool(
-                rec.location_dest_id and rec.location_dest_id.display_name.startswith("WH/IN/RETURN")
+            rec.is_return_reference = bool(
+                rec.reference and rec.reference.startswith("WH/IN/RETURN")
             )
