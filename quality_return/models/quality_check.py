ce79eed9c2f789744ea36b6b70dc7561b1398f08
chris mark cifra <ccifra@umindanao.edu.ph>
2025-09-12 02:23:54 +0800

    Update quality_check.py

--- quality_return/models/quality_check.py
+++ quality_return/models/quality_check.py
@@ -1,4 +1,19 @@
-from odoo import models
+# -*- coding: utf-8 -*-
+from odoo import models, fields
 
-class StockMoveLineViewer(models.Model):
+
+class StockMoveLine(models.Model):
     _inherit = "stock.move.line"
+
+    is_return_location = fields.Boolean(
+        string="Is Return",
+        compute="_compute_is_return",
+        store=False,
+    )
+
+    def _compute_is_return(self):
+        """Mark move lines whose destination location is a return location."""
+        for rec in self:
+            rec.is_return_location = bool(
+                rec.location_dest_id and rec.location_dest_id.display_name.startswith("WH/IN/RETURN")
+            )
