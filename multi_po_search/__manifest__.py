9224fbc8ae204687cf4275447c1d38007a302c42
chris mark cifra <ccifra@umindanao.edu.ph>
2025-09-25 23:01:10 +0800

    Add files via upload

---
+++ multi_po_search/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Multi   Sale PO # Search",
+     'version': '1.1',
+    'summary': 'Custom tokenized search on purchase_order field in Sale Orders',
+    'category': 'Sales',
+    'depends': ['sale'],
+    'data': [
+        'views/my_model_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
